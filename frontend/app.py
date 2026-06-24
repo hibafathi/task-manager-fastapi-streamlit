398cb8c1588c13e39001895978f24f0d2e495469
hibafathi <hibafathimam4747@gmail.com>
2026-06-24 16:32:45 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -7,7 +7,7 @@ from typing import Any
 import requests
 import streamlit as st
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL ="https://task-manager-fastapi-streamlit.onrender.com"
 REQUEST_TIMEOUT = 10
 
 
